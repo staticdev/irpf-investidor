584c982bcfbe878f2f13909962dc5d185e74ac31
staticdev <staticdev-support@proton.me>
2023-12-10 16:34:40 +0100

    Support Python 3.12

--- noxfile.py
+++ noxfile.py
@@ -20,7 +20,7 @@ except ImportError:
 
 
 package = "irpf_investidor"
-python_versions = ["3.11", "3.10"]
+python_versions = ["3.12", "3.11", "3.10"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
